3879a3b038eef785f4cd0b98646d0116937a58a1
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-07 02:00:59 +0300

    Create AssemblyWindow with Assembly container

---
+++ DotNetExplorer/AssemblyWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using System.Reflection;
+
+namespace DotNetExplorer
+{
+    public class AssemblyWindow : Window, IModelContainer<Assembly>
+    {
+        public AssemblyWindow(Assembly assembly = null)
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            Model = assembly;
+        }
+
+        private Assembly _model;
+        public Assembly Model
+        {
+            get => _model;
+            set
+            {
+                _model = value;
+                UpdateView();
+            }
+        }
+
+        private void UpdateView()
+        {
+
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ DotNetExplorer/IModelContainer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetExplorer
+{
+    public interface IModelContainer<T>
+    {
+        T Model { get; set; }
+    }
+}

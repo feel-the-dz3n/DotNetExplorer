342cd508dc799662a016fd7fc962d36f34f3f7b2
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-07 12:52:41 +0300

    Improve WelcomeWindow
    Add testing code
    Add correct assembly loading

--- DotNetExplorer/WelcomeWindow.xaml.cs
+++ DotNetExplorer/WelcomeWindow.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using System;
+using System.IO;
 using System.Reflection;
+using System.Text;
 
 namespace DotNetExplorer
 {
@@ -19,6 +23,10 @@ namespace DotNetExplorer
 
             if (version != null)
                 this.FindControl<TextBlock>("TbVersion").Text = "v" + version;
+
+#if DEBUG
+            new AssemblyWindow(TestAssembly.Fetch.Get()).Show();
+#endif
         }
 
         private async void LoadButtonClick(object sender, RoutedEventArgs e)
@@ -37,6 +45,34 @@ namespace DotNetExplorer
             dialog.Filters.AddRange(new FileDialogFilter[] { extNet, extAll });
 
             var files = await dialog.ShowAsync(this);
+
+            if (files.Length == 0)
+                return;
+
+            var loadedAssemblies = 0;
+
+            foreach (var file in files)
+            {
+                try
+                {
+                    var asm = Assembly.LoadFile(new FileInfo(file).FullName);
+                    new AssemblyWindow(asm).Show();
+                    loadedAssemblies++;
+                }
+                catch (Exception ex)
+                {
+                    var b = new StringBuilder();
+                    b.AppendLine("Unable to load assembly.");
+                    b.AppendLine();
+                    b.AppendLine("File: " + file);
+                    b.AppendLine("Exception: " + ex.GetType().Name);
+                    b.AppendLine("Message: " + ex.Message);
+                    await new MessageBox(b.ToString(), ".NET Explorer: Error").ShowDialog(this);
+                }
+            }
+
+            if(loadedAssemblies >= 1)
+                this.Close();
         }
 
         private void CloseButtonClick(object sender, RoutedEventArgs e)

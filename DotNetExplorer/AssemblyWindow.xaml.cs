dd433027a8b620e1c1038072d7aa82f32f14519d
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-07 12:51:04 +0300

    Improve AssemblyWindow

--- DotNetExplorer/AssemblyWindow.xaml.cs
+++ DotNetExplorer/AssemblyWindow.xaml.cs
@@ -1,18 +1,25 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 
 namespace DotNetExplorer
 {
     public class AssemblyWindow : Window, IModelContainer<Assembly>
     {
-        public AssemblyWindow(Assembly assembly = null)
+        public AssemblyWindow()
         {
             InitializeComponent();
 #if DEBUG
             this.AttachDevTools();
 #endif
+        }
+
+        public AssemblyWindow(Assembly assembly = null) : this()
+        {
             Model = assembly;
         }
 
@@ -29,7 +36,11 @@ namespace DotNetExplorer
 
         private void UpdateView()
         {
+            this.Title = $"{new FileInfo(Model.Location).Name} | .NET Explorer";
 
+            var items = Model.GetTypes().Select(x => new TextBlock() { Text = x.Name });
+
+            // this.FindControl<ListBox>("LbTest").Items = items;
         }
 
         private void InitializeComponent()
